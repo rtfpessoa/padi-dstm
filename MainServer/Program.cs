31228bbe14274116acf1cb987c8d0daf88c4c92a
Rafael Cortês <mrfyda@gmail.com>
2014-04-02 18:18:20 +0100

    new: main server

--- MainServer/Program.cs
+++ MainServer/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommonTypes;
+using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 
 namespace MainServer
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            TcpChannel channelServ = new TcpChannel(Config.REMOTE_MAINSERVER_PORT);
+            ChannelServices.RegisterChannel(channelServ, true);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(MainServer), Config.REMOTE_MAINSERVER_OBJ_NAME, WellKnownObjectMode.Singleton);
+
+            Console.WriteLine("Press <enter> to exit");
+            Console.ReadLine();
         }
     }
 }
