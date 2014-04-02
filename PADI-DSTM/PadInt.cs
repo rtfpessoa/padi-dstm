feb5e39413bfa71577042086a0d1d046aab3118d
Rafael Cortês <mrfyda@gmail.com>
2014-04-02 19:38:12 +0100

    fix compilation

--- PADI-DSTM/PadInt.cs
+++ PADI-DSTM/PadInt.cs
@@ -9,5 +9,14 @@ namespace PADI_DSTM
 {
     public class PadInt : IPadInt
     {
+        public int Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(int value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
