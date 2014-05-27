c5ab8d7c6611b15406d631a33455edbce519b28b
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2014-05-27 18:12:26 +0100

    update tests

--- 1000Objects/Program.cs
+++ 1000Objects/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using PADI_DSTM;
 
-class Cicle
+class TenThousand
 {
     static void Main(string[] args)
     {
@@ -17,15 +15,15 @@ class Cicle
             if ((args.Length > 0) && (args[0].Equals("C")))
             {
                 res = PadiDstm.TxBegin();
-                PadInt pi_a = PadiDstm.CreatePadInt(2);
-                PadInt pi_b = PadiDstm.CreatePadInt(2000000001);
-                PadInt pi_c = PadiDstm.CreatePadInt(1000000000);
-                pi_a.Write(0);
-                pi_b.Write(0);
+                for (int i = 0; i <= 10001; i++)
+                {
+                    PadInt pi_a = PadiDstm.CreatePadInt(i);
+                    pi_a.Write(i);
+                }
                 res = PadiDstm.TxCommit();
             }
             Console.WriteLine("####################################################################");
-            Console.WriteLine("Finished creating PadInts. Press enter for 300 R/W transaction cycle.");
+            Console.WriteLine("Finished creating PadInts. Press enter for sum transaction.");
             Console.WriteLine("####################################################################");
             Console.ReadLine();
         }
@@ -35,67 +33,72 @@ class Cicle
             Console.WriteLine("####################################################################");
             Console.WriteLine("AFTER create ABORT. Commit returned " + res + " . Press enter for abort and next transaction.");
             Console.WriteLine("####################################################################");
-            Console.ReadLine();
             PadiDstm.TxAbort();
         }
-        for (int i = 0; i < 300; i++)
+        try
         {
-            try
+            int sum = 0; PadInt pi_a;
+            res = PadiDstm.TxBegin();
+            for (int i = 0; i < 9999; i++)
             {
-                res = PadiDstm.TxBegin();
-                PadInt pi_d = PadiDstm.AccessPadInt(2);
-                PadInt pi_e = PadiDstm.AccessPadInt(2000000001);
-                PadInt pi_f = PadiDstm.AccessPadInt(1000000000);
-                int d = pi_d.Read();
-                d++;
-                pi_d.Write(d);
-                int e = pi_e.Read();
-                e++;
-                pi_e.Write(e);
-                int f = pi_f.Read();
-                f++;
-                pi_f.Write(f);
-                Console.Write(".");
-                res = PadiDstm.TxCommit();
-                if (res) { committed++; Console.Write("."); }
-                else
+                pi_a = PadiDstm.AccessPadInt(i);
+                if (args[0].Equals("D1F") && (i == 5000))
                 {
-                    aborted++;
-                    Console.WriteLine("$$$$$$$$$$$$$$ ABORT $$$$$$$$$$$$$$$$$");
+                    PadiDstm.Fail("tcp://localhost:2001/Server");
                 }
+                sum += pi_a.Read();
             }
-            catch (Exception e)
+            Console.WriteLine("sum= " + sum);
+            if (args[0].Equals("D1") || args[0].Equals("D1F"))
             {
-                Console.WriteLine("Exception: " + e.Message);
-                Console.WriteLine("####################################################################");
-                Console.WriteLine("AFTER create ABORT. Commit returned " + res + " . Press enter for abort and next transaction.");
-                Console.WriteLine("####################################################################");
-                PadiDstm.TxAbort();
-                aborted++;
+                pi_a = PadiDstm.AccessPadInt(10000);
+                pi_a.Write(sum);
             }
-
+            if (args[0].Equals("D2"))
+            {
+                pi_a = PadiDstm.AccessPadInt(10001);
+                pi_a.Write(sum);
+            }
+            res = PadiDstm.TxCommit();
+            if (res) { committed++; Console.Write("."); }
+            else
+            {
+                aborted++;
+                Console.WriteLine("$$$$$$$$$$$$$$ ABORT $$$$$$$$$$$$$$$$$");
+            }
         }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+            Console.WriteLine("####################################################################");
+            Console.WriteLine("AFTER create ABORT. Commit returned " + res + " . Press enter for abort and next transaction.");
+            Console.WriteLine("####################################################################");
+            PadiDstm.TxAbort();
+            aborted++;
+        }
+
+
         Console.WriteLine("####################################################################");
         Console.WriteLine("committed = " + committed + " ; aborted = " + aborted);
-        Console.WriteLine("Status after cycle. Press enter for verification transaction.");
+        Console.WriteLine("Press enter for status.");
         Console.WriteLine("####################################################################");
-        PadiDstm.Status();
         Console.ReadLine();
+        PadiDstm.Status();
+        Console.WriteLine("####################################################################");
+        Console.WriteLine("Press enter for verification transaction.");
+        Console.WriteLine("####################################################################");
 
         try
         {
             res = PadiDstm.TxBegin();
-            PadInt pi_g = PadiDstm.AccessPadInt(2);
-            PadInt pi_h = PadiDstm.AccessPadInt(2000000001);
-            PadInt pi_j = PadiDstm.AccessPadInt(1000000000);
+            PadInt pi_g = PadiDstm.AccessPadInt(10000);
             int g = pi_g.Read();
+            PadInt pi_h = PadiDstm.AccessPadInt(10001);
             int h = pi_h.Read();
-            int j = pi_j.Read();
             res = PadiDstm.TxCommit();
             Console.WriteLine("####################################################################");
-            Console.WriteLine("2 = " + g);
-            Console.WriteLine("2000000001 = " + h);
-            Console.WriteLine("1000000000 = " + j);
+            Console.WriteLine("sum D1 = " + g);
+            Console.WriteLine("sum D2 = " + h);
             Console.WriteLine("Status post verification transaction. Press enter for exit.");
             Console.WriteLine("####################################################################");
             PadiDstm.Status();
